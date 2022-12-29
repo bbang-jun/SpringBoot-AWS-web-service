ee848cf58bd628bd83ce5fe55db60e9e19737c40
bbangjun <lucid9dawn@gmail.com>
2022-12-29 19:16:09 +0900

    [완료] JPA, H2의 기능 설정 진행 및 출력 쿼리 로그를 MySQL 버전으로 변경 완료 (100p)

---
+++ src/main/java/com/bbangjun/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.bbangjun.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> { // 94p
+
+}
