d5dbb1c4b9b56bc79be967df18adadce552bcd55
bbangjun <lucid9dawn@gmail.com>
2023-01-06 19:44:10 +0900

    refactor(IndexController): 클린 코드를 위해 반복되는 부분을 @LoginUser로 리팩토링 (5.4_200p)

---
+++ src/main/java/com/bbangjun/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.bbangjun.config;
+
+import com.bbangjun.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer { // 198p
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/bbangjun/web/IndexController.java
+++ src/main/java/com/bbangjun/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.bbangjun.web;
 
+import com.bbangjun.config.auth.LoginUser;
+import com.bbangjun.config.auth.dto.SessionUser;
 import com.bbangjun.service.posts.PostsService;
 import com.bbangjun.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,20 +10,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService; // 150p
+    private final HttpSession httpSession; // 190p
 
     @GetMapping("/posts/save")
     public String postsSave(){ // /posts/save 호출 시 posts-save.mustache를 호출하는 메서드
         return "posts-save";
     }
 
+//    @GetMapping("/")
+//    public String index(Model model){ // 150p
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+//        if(user!=null){
+//            model.addAttribute("userName", user.getName());
+//        }
+//        return "index";
+//    }
     @GetMapping("/")
-    public String index(Model model){ // 150p
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts", postsService.findAllDesc()); // 199p
+
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
